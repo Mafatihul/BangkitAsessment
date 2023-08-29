ed863d1bd2958bef65592a267333abd2a1aeb529
Mafatihul <89466628+Mafatihul@users.noreply.github.com>
2023-08-29 14:11:47 +0700

    Menambahkan file syslog.txt yang berisi log message (beberapa saja) File ini nanti digunakan untuk input (mencari error dari log message dalam file ini)
    Menambahkan file ticky.find yang berisi source code yang akan digunakan untuk menyelesaikan task.
    
    Project :
    Kita diberikan sebuah file berisi log message dengan berbagai informasi.
    Dari file tersebut kita diminta untuk mencari pesan error dan jenis errornya.
    Pesan error tersebut akan dihitung berapa kemunculan setiap jenisnya.
    Tugas ini dilaporkan dalam bentuk csv dengan diurutkan berdasarkan kemunculan jenis error terbanyak.
    
    Tugas kedua adalah menemukan aktivitas setiap username.
    Untuk setiap username akan dihitung berapa banyak mendapat ERROR ataupun INFO.
    Tugas ini dilaporkan dalam bentuk file csv dengan diurutkan sesuai abjad username.

---
+++ InteractwithOS_course/ticky_find.py
@@ -0,0 +1,44 @@
+# import library
+import sys
+import csv
+import re
+import subprocess
+
+# inisialisasi dictionary 
+# untuk menampung jumlah eror berdasarkan jenisnya
+# type_error_count = dict()
+# count_error = int()
+# error_type = []
+# username = []
+
+#  with open('syslog.txt', 'r') as f:
+#     for line in f.readlines():
+#       message = re.search(r"ERROR ([\w\s]*).\(([\w]*)\)", line)
+#        if message != None:
+#           type_error_count[message[1]]
+#           # username.append(message[2])
+#     f.close()
+
+# print(type_error_count)
+
+def get_error(file):
+   err_messages = []
+   count_err = {}
+   with open(file, 'r') as f:
+      for line in f.readlines():
+        messages = re.search(r"ERROR ([\w\s]*)", line)
+        if messages != None:
+           err_messages.append(messages[1])
+      f.close()
+      for err in err_messages:
+         if err in count_err:
+            count_err[err] += 1
+         else:
+            count_err[err] = 1
+   return count_err
+
+print(get_error('syslog.txt'))
+
+# def get_users(file):
+
+# def generate_report()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BangkitAsessment
  This is all the individual assesment from bangkit academy
+ Repositori ini berisi semua tugas project individu yang didapat saat mengikuti Bangkit Academy 2023 Machine Learning Path. Course yang diikuti meliputi
+ 1. Google IT Automation with Python
+    - Crash Course on Python
+    - Using Python to Interact with the OS
+    - Introduction to Git and GitHub  
+ 2. Google Data Analytics
